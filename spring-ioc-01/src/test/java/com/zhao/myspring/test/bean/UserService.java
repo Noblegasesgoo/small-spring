871d4c732eac96199af3aada4d349d0c6aeca57a
Noblegasesgoo <2632816823@qq.com>
2022-08-28 18:23:15 +0800

    测试bean

---
+++ spring-ioc-01/src/test/java/com/zhao/myspring/test/bean/UserService.java
@@ -0,0 +1,14 @@
+package com.zhao.myspring.test.bean;
+
+/**
+ * @author noblegasesgoo
+ * @version 0.0.1
+ * @date 2022/7/26 16:31
+ * @description
+ */
+public class UserService {
+
+    public void queryUserInfo(){
+        System.out.println("查询用户信息");
+    }
+}
