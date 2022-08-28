b1bb739ebe69dfe05559c1491c9e943db0940890
Noblegasesgoo <2632816823@qq.com>
2022-08-28 18:31:32 +0800

    测试bean

---
+++ spring-ioc-02/src/test/java/com/noblegasesgoo/springframwork/test/MyService.java
@@ -0,0 +1,16 @@
+package com.noblegasesgoo.springframwork.test;
+
+/**
+ * @author: noblegasesgoo
+ * @CreateDate: 2022年08月28日 17:35
+ * @ProjectName: small-spring-private
+ * @version: 0.0.1
+ * @FileName: MyService
+ * @Description:
+ */
+public class MyService {
+
+    public void doTest() {
+        System.out.println("测试成功！");
+    }
+}
